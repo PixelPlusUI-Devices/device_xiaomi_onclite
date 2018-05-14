333ade4a74b70ff4c9d8245de2870c6f72988060
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    onclite: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I9ab8b076b4bede70c368c198960ceabc68dad65b

--- device.mk
+++ device.mk
@@ -88,8 +88,7 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libqcompostprocbundle \
-    tinymix
+    libqcompostprocbundle
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
