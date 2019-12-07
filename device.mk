77557e7c1c4d87772313afabd20081e956e6306b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-07 22:31:24 +0100

    onclite: Introduce Safailnet

--- device.mk
+++ device.mk
@@ -299,7 +299,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
