909c2bf52791acc26a9f95e278aedc6201a9db42
ragy <wad.moskla@gmail.com>
2020-01-10 15:13:24 +0200

    onclite: PixelExperience BringUp

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -12,11 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
-# Bootanimation res
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_onclite
