a5755f46d07ca4995f217f083b0c4e18053f2404
ragy <wad.moskla@gmail.com>
2020-01-10 15:36:11 +0200

    onclite: enable optimize refresh feature

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -98,6 +98,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sdm.debug.disable_skip_validate=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=0 \
+    vendor.display.enable_optimize_refresh=1 \
     vendor.display.svi.config=1 \
     vendor.display.svi.config_path=/system/etc/display/SVIConfig.xml \
     vendor.gralloc.enable_fb_ubwc=1
