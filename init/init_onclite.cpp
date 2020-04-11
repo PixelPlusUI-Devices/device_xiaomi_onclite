4cec7878944c17f2263b212146516d5ffe8aab9e
prabhat774 <34837643+prabhat774@users.noreply.github.com>
2020-04-11 12:26:50 +0200

    onclite: Update fingerprint to coral April 2020

--- init/init_onclite.cpp
+++ init/init_onclite.cpp
@@ -59,5 +59,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "onc-user 9 PKQ1.181021.001 V10.3.4.0.PFLMIXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys");
 }
