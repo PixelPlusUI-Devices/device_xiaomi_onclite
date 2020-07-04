ed544f04d09923173c8913578d85bcd934309b67
SonalSingh18 <sonal.singh.19993@gmail.com>
2020-07-04 22:19:20 +0530

    violet: Import VioletParts
    Features:
    • Selinux Toggle
    • Toggle Usb Fast Charge
    
    Change-Id: I2947aa62b78b1150abde3484b7d3af5d97267f80
    Co-authored-by: StyloGey <stylogey@gmail.com>
    Co-authored-by: Rizky Benggolo <travarilo@gmail.com>
    Co-authored-by: Genkzsz11 <genkzsz11@gmail.com>
    Co-authored-by: smokey18 <kmboplays@gmail.com>
    Co-authored-by: ronaxdevil <pratabidya.007@gmail.com>
    Co-authored-by: SiddharthBharadwaj <45765901+SiddharthBharadwaj@users.noreply.github.com>
    Co-authored-by: Saurabh Charde <saurabhchardereal@gmail.com>
    
    Adapted from: https://github.com/PainKiller3/device_xiaomi_sdm845-common/commit/1510fbe2049ef83b53fe971a6441ee26c47c3208
    by: @athul05
    
    Signed-off-by: athul05 <athuldineshan5@gmail.com>
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- device.mk
+++ device.mk
@@ -142,6 +142,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    VioletParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
@@ -389,6 +393,7 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+<<<<<<< HEAD
 # VNDK
 PRODUCT_PACKAGES += \
     vndk_package
@@ -398,6 +403,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/permissions/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
+=======
+#VioletParts init
+PRODUCT_PACKAGES += \
+    init.violetparts.sh \
+    init.violetparts.rc
+>>>>>>> 55f0403c... violet: Import VioletParts
 
 # Wifi
 PRODUCT_PACKAGES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -102,3 +102,18 @@ LOCAL_SRC_FILES    := bin/init.qcom.post_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.violetparts.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.violetparts.rc
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.violetparts.sh
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.violetparts.sh
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
