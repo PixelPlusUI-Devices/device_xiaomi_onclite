006ff4fe8e5cc34120a045b7e377c3b4246bfc8c
Stallix <joey@evolution-x.org>
2021-02-15 14:58:50 +0200

    onclite: Update redfin fingerprint to Jan 2021

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -27,9 +27,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210205.004 7038034 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
